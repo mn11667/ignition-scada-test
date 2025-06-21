b34bf5bf411861edb7a5754f127d00accd08a128
mn11667 <mn11github@gmail.com>
2025-06-22 01:01:00 +0545

    Add Sparkplug B protobuf and update app.py with Sparkplug publishing

--- mqtt_publisher.py
+++ mqtt_publisher.py
@@ -1,19 +1,18 @@
 import paho.mqtt.client as mqtt
 import time
 import random
-import json
 
 client = mqtt.Client()
-client.connect("localhost", 1883, 60)
+client.connect("127.0.0.1", 1883, 60)
+client.loop_start()
 
 while True:
     temperature = round(random.uniform(20.0, 40.0), 2)
     pressure = round(random.uniform(1.0, 2.0), 2)
-    data = {
-        "temperature": temperature,
-        "pressure": pressure
-    }
-    client.publish("sensors/data", json.dumps(data))
-    print("Published:", data)
-    time.sleep(2)
 
+    client.publish("sensors/data/temperature", str(temperature))
+    client.publish("sensors/data/pressure", str(pressure))
+
+    print(f"Published → temperature: {temperature}, pressure: {pressure}")
+    time.sleep(2)
+    
